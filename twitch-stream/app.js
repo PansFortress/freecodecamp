8bfe7a13d05e897c36e35192a4d4be120bf26892
PansFortress <japan8789@gmail.com>
2017-08-09 16:58:13 -0400

    Setup AJAX calls

--- twitch-stream/app.js
+++ twitch-stream/app.js
@@ -1,14 +1,20 @@
+//TODO: Transform channelNames to an array of Objects and start populating the responses of channel and stream to the object to help draw on render later
+var channelNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
 window.onload = function(){
     console.log("Loaded");
-    getChannelData('freecodecamp')
-    .done(function(data){
-        console.log(data);
-    });
+
+    for(var i = 0; i < channelNames.length; i++){
+        getChannelData('channels',channelNames[i])
+        .done(function(data){
+            console.log(data);
+        });
+    }
 };
 
-var getChannelData = function(channelName){
+var getChannelData = function(type, channelName){
     var settings = {
-        url: 'https://api.twitch.tv/kraken/channels/' + channelName,
+        url: 'https://api.twitch.tv/kraken/' + type + '/' + channelName,
         headers:{
             'Client-ID': 'ay5sj1z6z10jovq449fit92yyvv9pe',
         },
